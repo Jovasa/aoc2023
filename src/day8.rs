d905fc494a4e462f1a568300c64ed1a51b4fc76b
Jovasa <jovasa46@gmail.com>
2024-02-11 10:53:02 +0200

    LCM because reasons

--- src/day8.rs
+++ src/day8.rs
@@ -40,24 +40,68 @@ fn main() {
     }
     println!("num_steps: {}", num_steps);
 
-    let mut current_positions = starts;
-    let mut num_steps = 0;
-    loop {
-        let mut next_positions = Vec::new();
-        let dir = directions[num_steps % directions.len()];
-        for current in &current_positions {
-            let (left, right) = dir_map.get(current).unwrap();
+    // let mut current_positions = starts;
+    // let mut num_steps = 0;
+    // loop {
+    //     let mut next_positions = Vec::new();
+    //     let dir = directions[num_steps % directions.len()];
+    //     for current in &current_positions {
+    //         let (left, right) = dir_map.get(current).unwrap();
+    //         if dir == 'L' {
+    //             next_positions.push(left.to_owned());
+    //         } else {
+    //             next_positions.push(right.to_owned());
+    //         }
+    //     }
+    //     if current_positions.iter().all(|x| x.ends_with("Z")) {
+    //         println!("num_steps: {}", num_steps);
+    //         return;
+    //     }
+    //     current_positions = next_positions;
+    //     num_steps += 1;
+    // }
+    let mut all_steps = Vec::new();
+    for start in starts {
+        let mut current = start;
+        let mut num_steps = 0;
+        while !current.ends_with("Z") {
+            let (left, right) = dir_map.get(&current).unwrap();
+            let dir = directions[num_steps % directions.len()];
             if dir == 'L' {
-                next_positions.push(left.to_owned());
+                current = left;
             } else {
-                next_positions.push(right.to_owned());
+                current = right;
             }
+            num_steps += 1;
         }
-        if current_positions.iter().all(|x| x.ends_with("Z")) {
-            println!("num_steps: {}", num_steps);
-            return;
-        }
-        current_positions = next_positions;
-        num_steps += 1;
+        all_steps.push(num_steps);
     }
+    let a = lcm(all_steps);
+    println!("lcm: {}", a);
+}
+
+fn least_common_multiple(p0: Vec<usize>) -> usize {
+    if p0.is_empty() {
+        return 0;
+    }
+    let mut p = p0;
+    p.sort();
+    let mut lcm = p[0];
+    for &i in &p[1..] {
+        lcm = lcm * i / gcd(lcm, i);
+    }
+    lcm
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    a
+}
+
+fn lcm(v: Vec<usize>) -> usize {
+    least_common_multiple(v)
 }
