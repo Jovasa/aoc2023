b9cf3b86c044906a97d9622cfe914f46339fa7fc
Jovasa <jovasa46@gmail.com>
2024-02-13 20:31:27 +0200

    Day10 second

--- src/day10.rs
+++ src/day10.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
-use crate::PipePiece::Vertical;
+use crate::PipePiece::{Ground, Horizontal, Vertical};
 
-#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+#[derive(Clone, Copy, Eq, PartialEq, Debug, Hash)]
 #[repr(u8)]
 enum PipePiece {
     Vertical = b'|',
@@ -11,6 +11,7 @@ enum PipePiece {
     DownRightCorner = b'F',
     DownLeftCorner = b'7',
     Ground = b'.',
+    Outside = b'o',
 }
 
 impl From<u8> for PipePiece {
@@ -23,17 +24,25 @@ impl From<u8> for PipePiece {
             b'F' => PipePiece::DownRightCorner,
             b'7' => PipePiece::DownLeftCorner,
             b'.' => PipePiece::Ground,
+            b'o' => PipePiece::Outside,
             _ => panic!("Invalid character"),
         }
     }
+
 }
+ impl PipePiece {
+     fn as_char(&self) -> char {
+         *self as u8 as char
+     }
+ }
+
 
 fn main() {
     let input = std::fs::read_to_string("data/day10.txt").unwrap();
     let  lines = input.lines();
 
     let mut grid = Vec::new();
-    let mut start = (0, 0);
+    let mut start = (1, 1);
     for (y, line) in lines.enumerate() {
         let mut row = Vec::new();
         for (x, c) in line.bytes().enumerate() {
@@ -51,31 +60,62 @@ fn main() {
     let mut visited = HashSet::new();
     visited.insert(start);
     let mut last_two = vec![];
-    if grid[start.1 - 1][start.0] == PipePiece::Horizontal ||
+    let mut possible_starts = HashSet::new();
+    possible_starts.insert(PipePiece::Horizontal);
+    possible_starts.insert(PipePiece::Vertical);
+    possible_starts.insert(PipePiece::UpRightCorner);
+    possible_starts.insert(PipePiece::UpLeftCorner);
+    possible_starts.insert(PipePiece::DownRightCorner);
+    possible_starts.insert(PipePiece::DownLeftCorner);
+
+    if grid[start.1 - 1][start.0] == PipePiece::Vertical ||
         grid[start.1 - 1][start.0] == PipePiece::DownRightCorner ||
         grid[start.1 - 1][start.0] == PipePiece::DownLeftCorner {
         visited.insert((start.0, start.1 - 1));
         last_two.push((start.0, start.1 - 1));
+        let mut starts_to_here = HashSet::new();
+        starts_to_here.insert(PipePiece::Vertical);
+        starts_to_here.insert(PipePiece::UpRightCorner);
+        starts_to_here.insert(PipePiece::UpLeftCorner);
+        possible_starts = possible_starts.intersection(&starts_to_here).cloned().collect();
     }
-    if grid[start.1 + 1][start.0] == PipePiece::Horizontal ||
+    if grid[start.1 + 1][start.0] == PipePiece::Vertical ||
         grid[start.1 + 1][start.0] == PipePiece::UpRightCorner ||
         grid[start.1 + 1][start.0] == PipePiece::UpLeftCorner {
         visited.insert((start.0, start.1 + 1));
         last_two.push((start.0, start.1 + 1));
+        let mut starts_to_here = HashSet::new();
+        starts_to_here.insert(PipePiece::Vertical);
+        starts_to_here.insert(PipePiece::DownRightCorner);
+        starts_to_here.insert(PipePiece::DownLeftCorner);
+        possible_starts = possible_starts.intersection(&starts_to_here).cloned().collect();
     }
-    if grid[start.1][start.0 - 1] == PipePiece::Vertical ||
+    if grid[start.1][start.0 - 1] == PipePiece::Horizontal ||
         grid[start.1][start.0 - 1] == PipePiece::DownRightCorner ||
         grid[start.1][start.0 - 1] == PipePiece::UpRightCorner {
         visited.insert((start.0 - 1, start.1));
         last_two.push((start.0 - 1, start.1));
+        let mut starts_to_here = HashSet::new();
+        starts_to_here.insert(PipePiece::Horizontal);
+        starts_to_here.insert(PipePiece::UpLeftCorner);
+        starts_to_here.insert(PipePiece::DownLeftCorner);
+        possible_starts = possible_starts.intersection(&starts_to_here).cloned().collect();
     }
-    if grid[start.1][start.0 + 1] == PipePiece::Vertical ||
+    if grid[start.1][start.0 + 1] == PipePiece::Horizontal ||
         grid[start.1][start.0 + 1] == PipePiece::DownLeftCorner ||
         grid[start.1][start.0 + 1] == PipePiece::UpLeftCorner {
         visited.insert((start.0 + 1, start.1));
         last_two.push((start.0 + 1, start.1));
+        let mut starts_to_here = HashSet::new();
+        starts_to_here.insert(PipePiece::Horizontal);
+        starts_to_here.insert(PipePiece::UpRightCorner);
+        starts_to_here.insert(PipePiece::DownRightCorner);
+        possible_starts = possible_starts.intersection(&starts_to_here).cloned().collect();
     }
-    assert!(last_two.len() == 2);
+    assert_eq!(possible_starts.len(), 1);
+    grid[start.1][start.0] = possible_starts.iter().next().unwrap().clone();
+
+    assert_eq!(last_two.len(), 2);
     loop {
         let mut next_two = vec![];
         for &position in last_two.iter() {
@@ -150,4 +190,75 @@ fn main() {
         last_two = next_two;
     }
     println!("Visited: {}", visited.len() / 2);
+    let mut up_sampled_grid = Vec::new();
+    for (y, row) in grid.iter().enumerate() {
+        let mut new_row = Vec::new();
+        for (x, &c) in row.iter().enumerate() {
+            if visited.contains(&(x, y)) {
+                new_row.push(c);
+                if c == Horizontal || c == PipePiece::DownRightCorner || c == PipePiece::UpRightCorner {
+                    new_row.push(Horizontal);
+                }
+                else {
+                    new_row.push(Ground);
+                }
+            }
+            else {
+                new_row.push(Ground);
+                new_row.push(Ground);
+            }
+        }
+        up_sampled_grid.push(new_row);
+        let mut new_row = Vec::new();
+        for (x, &c) in row.iter().enumerate() {
+            if visited.contains(&(x, y)) {
+                if c == Vertical || c == PipePiece::DownLeftCorner || c == PipePiece::DownRightCorner {
+                    new_row.push(Vertical);
+                }
+                else {
+                    new_row.push(Ground);
+                }
+                new_row.push(Ground);
+            }
+            else {
+                new_row.push(Ground);
+                new_row.push(Ground);
+            }
+        }
+        up_sampled_grid.push(new_row);
+    }
+
+    let mut flood = vec![(0,0)];
+    while !flood.is_empty() {
+        let mut next_flood = vec![];
+        for &position in flood.iter() {
+            let (x, y) = position;
+            if up_sampled_grid[y][x] == Ground {
+                up_sampled_grid[y][x] = PipePiece::Outside;
+                if x > 0 {
+                    next_flood.push((x - 1, y));
+                }
+                if y > 0 {
+                    next_flood.push((x, y - 1));
+                }
+                if x < up_sampled_grid[y].len() - 1 {
+                    next_flood.push((x + 1, y));
+                }
+                if y < up_sampled_grid.len() - 1 {
+                    next_flood.push((x, y + 1));
+                }
+            }
+        }
+        flood = next_flood;
+    }
+
+    let mut inside = 0;
+    for row in up_sampled_grid.iter().step_by(2) {
+        for &c in row.iter().step_by(2) {
+            if c == PipePiece::Ground {
+                inside += 1;
+            }
+        }
+    }
+    println!("Inside: {}", inside);
 }
